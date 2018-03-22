03a8c0e32bf24a43afab912f2a18c761952aa1be
alisa <alisa@selfnet.de>
2018-03-22 22:09:34 +0100

    add smt

--- multivault/integrities/check.py
+++ multivault/integrities/check.py
@@ -2,20 +2,51 @@ import pgpy
 from ansible.parsing.dataloader import DataLoader
 from ansible.inventory.manager import InventoryManager
 
+import ansible.playbook.play as play
+import yaml
+from multivault.utilities import util_crypt
+
+INVENTORY_FILE = '/home/alisa/git/playbooks/inventory.ini'
+IGNORED = ['ubuntu', 'debian', 'ubuntu_host', 'debian_host']
+INVENTORY = InventoryManager(loader=DataLoader(),
+                             sources=[INVENTORY_FILE])
+
 
 def read_message(file_to_read):
     message = pgpy.PGPMessage.from_file(file_to_read)
     dir(message)
     print(message.encrypters)
-    
-def parse_ansible(inventory_file):
-    
-    data_loader = DataLoader()
-    inventory = InventoryManager(loader = data_loader,
-                             sources=[inventory_file])
 
-    print(inventory.get_groups_dict()['testgroup1'])
+
+
+def parse_play(play_file):
+
+    with open(play_file, mode = "r") as playbook:
+        playbook=yaml.load(playbook)
+       # print(playbook)
+        for task in playbook:
+            # print(task["hosts"])
+            hosts=task['hosts'].lower().split(',')
+            hosts=[host.strip() for host in hosts]
+            div=set(hosts)-set(IGNORED)
+            div=list(div)
+            print(match(div))
+def match(groups):
+    hosts = []
+    for group in groups :
+        if group.startswith('!'):
+            pass
+        else :
+            try:
+                hosts.append(INVENTORY.get_groups_dict()[group])
+            except Exception:
+                pass
+    
+    return util_crypt.flatten(hosts)
+    
+
+
 
 if __name__ == '__main__':
     read_message('/home/alisa/Downloads/test.pw.gpg')
-    parse_ansible('/home/alisa/Downloads/inventory_test')
+    parse_play('/home/alisa/git/playbooks/all.yml')
