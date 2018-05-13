6827c23310681e876df669327d84cca7c6bc3367
Cellebyte <marcel.fest@live.de>
2018-05-13 16:07:04 +0200

    added dependency match for meta.yml for one dependency stack

--- multivault/integrities/check.py
+++ multivault/integrities/check.py
@@ -1,6 +1,8 @@
 import configparser
 import yaml
 import os
+import copy
+import json
 from multivault.utilities import util_check
 from multivault.utilities import util_crypt
 from multivault.utilities import util_ldap
@@ -15,6 +17,8 @@ except ImportError:
     print("The integrity module relies on ansible!")
     print("\tpip3 install ansible")
     exit(1)
+
+INVENTORY = None
 
 
 class bcolors:
@@ -37,9 +41,11 @@ def get_all_users_and_subkeys():
         authorized = {}
     for user, key in authorized:
         if not isinstance(key, str):
-            results[user] = []
+            results[user] = {}
+            results[user]['key'] = key
+            results[user]['subkeys'] = []
             for name, _ in dict(key.subkeys).items():
-                results[user].append(name)
+                results[user]['subkeys'].append(name)
     return results
 
 
@@ -51,6 +57,7 @@ def init(workdir='/home/cellebyte/git/selfnet/playbooks'):
     DEFAULT_INVENTORY = None
     ROLES_PATH = None
     USER_SUBKEYS = get_all_users_and_subkeys()
+    # USER_SUBKEYS = None
     config.read(ansible_cfg)
     for section in config:
         for key_pair in config[section]:
@@ -75,20 +82,33 @@ def init(workdir='/home/cellebyte/git/selfnet/playbooks'):
 def checkout_information(MAPPING, DEPENDENCY_TREE, workdir=None, roles_path=None):
     results = []
     for role, hosts in MAPPING.items():
-        roles = DEPENDENCY_TREE[role]
-        [ roles.append(role) for role in roles if DEPENDENCY_TREE[role] and not role in roles]
         gpg_path = os.path.join(workdir, roles_path, role, 'gpg')
         if os.path.exists(gpg_path):
             results.append(check_with_structure(role, hosts, gpg_path))
-        for dep_role in roles:
-            gpg_path = os.path.join(workdir, roles_path, dep_role, 'gpg')
-            if os.path.exists(gpg_path):
-                results.append(check_with_structure(dep_role, hosts, gpg_path))
+    roles_with_gpg = [result['role'] for result in results]
+    for result in results:
+        role = result['role']
+        hosts = result['hosts']
+        dependency_roles = DEPENDENCY_TREE[role]
+        for dependency_role in dependency_roles:
+            if dependency_role in roles_with_gpg:
+                gpg_role = [result for result in results if result['role'] == dependency_role][0]
+                pprint(gpg_role)
+                for _, information in gpg_role['files'].items():
+                    for host in hosts:
+                        if host not in information['hosts'] and hosts == information['hosts']:
+                            information['hosts'].append(host)
+                pprint(role)
+                print('---------------------------')
+                pprint(gpg_role)
     return results
 
 
 def check_with_structure(role, hosts, gpg_path):
     results = {}
+    results['role'] = role
+    results['hosts'] = hosts
+    results['files'] = {}
     for path, _, files in os.walk(gpg_path):
         for file in files:
             file_path = os.path.join(path, file)
@@ -97,74 +117,81 @@ def check_with_structure(role, hosts, gpg_path):
                     gpg_path+os.sep, file_path)
                 splitted_path = minified_path.split(os.sep)
                 to_be_encrypted_for = []
+                results['files'][file_path] = {}
                 if len(splitted_path) > 1:
                     for host in splitted_path:
+                        # host is an encrypted file and not a host
                         if host.endswith('.gpg') and os.path.isfile(file_path):
-                            results[file_path] = to_be_encrypted_for
-                        elif util_check.is_valid_hostname(host):
+                            results['files'][file_path]['hosts'] = to_be_encrypted_for
+                        elif util_check.is_valid_hostname(host) and host in util_check.match(['all'], INVENTORY):
                             to_be_encrypted_for.append(host)
                         else:
-                            print('Unknown')
+                            results['files'][file_path]['hosts'] = hosts
+                            break
                 else:
-                    results[file_path] = hosts
+                    results['files'][file_path]['hosts'] = hosts
     return results
 
 
 def get_encrypters_from_file(informations):
-    results = {}
     for information in informations:
-        for path, _ in information.items():
-            results[path] = list(util_check.read_message(path))
-    return results
+        for path, _ in information['files'].items():
+            information['files'][path]['encrypters'] = list(
+                util_check.read_message(path))
+    return informations
 
 
 def get_encrypters_from_ldap(informations, USER_SUBKEYS):
-    results = {}
     for information in informations:
-        for path, hosts in information.items():
+        for path in information['files'].keys():
             authorized = util_ldap.get_authorized(
-                [host.split('.')[0] for host in hosts])
+                [host.split('.')[0] for host in information['files'][path]['hosts']])
             if not authorized:
                 authorized = {}
-            results[path] = []
+            information['files'][path]['sudoers'] = []
             for user, _ in authorized:
                 for uid, keys in USER_SUBKEYS.items():
                     if uid == user:
-                        results[path].append({uid: keys})
-    return results
+                        information['files'][path]['sudoers'].append(
+                            {uid: keys})
+    return informations
 
 
-def crossover(IS_ENCRYPTED_FOR, SHOULD_BE_ENCRYPTED_FOR, USER_SUBKEYS):
-    for path, user_keys in SHOULD_BE_ENCRYPTED_FOR.items():
-        keys = list(IS_ENCRYPTED_FOR[path])
-        for user_key in user_keys:
-            for user, ikeys in user_key.items():
-                matched = False
-                for ikey in ikeys:
-                    if ikey in keys:
-                        matched = True
-                if not matched:
-                    print(
-                        bcolors.WARNING + 'WARNING\t:::\t{} not encrypted for {}'.format(path, user))
-                else:
-                    print(
-                        bcolors.OKGREEN + 'OK\t:::\t{} correctly encrypted for {}'.format(path, user))
-                    for ikey in ikeys:
-                        try:
-                            keys.remove(ikey)
-                        except ValueError:
-                            pass
-        if len(keys) > 0:
-            for key in keys:
-                user = get_user(key, USER_SUBKEYS)
-                if not user:
-                    print(
-                        bcolors.WARNING + 'WARNING\t:::\t{} encrypted for unknown key {}'.format(path, key))
-                else:
-                    print(
-                        bcolors.FAIL + 'ERROR\t:::\t{} encrypted for user {}, no longer assigned on this file.(please fix!)'.format(path, user))
-
-    pass
+def crossover(informations, USER_SUBKEYS):
+    for information in informations:
+        print(bcolors.OKBLUE +
+              'INFO\t:::\tCheck Encryption for role >> {} <<'.format(information['role']))
+        for path, file_information in information['files'].items():
+            keys = list(copy.deepcopy(file_information['encrypters']))
+            print(bcolors.OKBLUE +
+                  '\tINFO\t:::\tCheck for hosts >> {} <<'.format(file_information['hosts']))
+            filename = '.../' + str(path.split(os.sep)[-1])
+            for sudoer in file_information['sudoers']:
+                for user, key_information in sudoer.items():
+                    matched = False
+                    for sub_key in key_information['subkeys']:
+                        if sub_key in keys:
+                            matched = True
+                    if not matched:
+                        print(
+                            bcolors.WARNING + '\tWARNING\t:::\t{} not encrypted for user >> {} <<'.format(filename, user))
+                    else:
+                        print(
+                            bcolors.OKGREEN + '\tOK\t:::\t{} correctly encrypted for user >> {} <<'.format(filename, user))
+                        for sub_key in key_information['subkeys']:
+                            try:
+                                keys.remove(sub_key)
+                            except ValueError:
+                                pass
+            if len(keys) > 0:
+                for key in keys:
+                    user = get_user(key, USER_SUBKEYS)
+                    if not user:
+                        print(
+                            bcolors.WARNING + '\tWARNING\t:::\t{} encrypted for unknown key >> {} <<'.format(filename, key))
+                    else:
+                        print(
+                            bcolors.FAIL + '\tERROR\t:::\t{} wrong encrypted for user >> {} <<(please fix!)'.format(filename, user))
 
 
 def get_user(key, USER_SUBKEYS):
@@ -175,21 +202,24 @@ def get_user(key, USER_SUBKEYS):
 
 
 def check(workdir=None, playbook='all.yml'):
+    global INVENTORY
     if not workdir:
         workdir, INVENTORY, roles_path, USER_SUBKEYS = init()
     else:
         return "Not Implemented"
+    # pprint(USER_SUBKEYS)
     playbook_file = os.path.join(workdir, playbook)
     mapping = util_check.parse_play(playbook_file, INVENTORY=INVENTORY)
     MAPPING = util_check.look_for_dependencies(mapping, workdir, roles_path)
     DEPENDENCY_TREE = util_check.build_dependency_tree(workdir, util_check.get_roles(workdir, roles_path=roles_path), roles_path=roles_path)
     pprint(DEPENDENCY_TREE)
     FILE_HOSTS = checkout_information(
-        MAPPING, DEPENDENCY_TREE, workdir=workdir, roles_path=roles_path)
+         MAPPING, DEPENDENCY_TREE, workdir=workdir, roles_path=roles_path)
     IS_ENCRYPTED_FOR = get_encrypters_from_file(FILE_HOSTS)
     SHOULD_BE_ENCRYPTED_FOR = get_encrypters_from_ldap(
-        FILE_HOSTS, USER_SUBKEYS)
-    crossover(IS_ENCRYPTED_FOR, SHOULD_BE_ENCRYPTED_FOR, USER_SUBKEYS)
+        IS_ENCRYPTED_FOR, USER_SUBKEYS)
+    # pprint(SHOULD_BE_ENCRYPTED_FOR)
+    crossover(SHOULD_BE_ENCRYPTED_FOR, USER_SUBKEYS)
 
 
 if __name__ == '__main__':
